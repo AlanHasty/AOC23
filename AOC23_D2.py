6e4426cedb73a79cd2518664a2d99173b3970769
Alan Hasty <alan.hasty@gmail.com>
2023-12-03 13:19:16 -0600

    Day 2 Part 2 complete

--- AOC23_D2.py
+++ AOC23_D2.py
@@ -18,6 +18,50 @@ def split_game_data(line, debug):
                 sep='\n')
     return (game_num, round_data)
 
+def game_powers(g_data, debug):
+    (id, rounds) = g_data
+    red = re.compile(r"(\d+)red")
+    blue = re.compile(r"(\d+)blue")
+    green = re.compile(r"(\d+)green")
+    red_tot = blue_tot = green_tot = 0
+
+    for indx, rd in enumerate(rounds):
+        red_cnt = 0
+        blue_cnt = 0
+        green_cnt = 0
+        colors = rd.split(",")
+
+        for cl in colors:
+            c = cl.replace(" ", "")
+            if c != None:
+                if "red" in c:
+                    m = red.match(c)
+                    if m != None:
+                        red_cnt = int(m.group(1))
+                elif "blue" in c:
+                    m = blue.match(c)
+                    if m != None:
+                        blue_cnt = int(m.group(1))
+                elif "green" in c:
+                    m = green.match(c)
+                    if m != None:
+                        green_cnt = int(m.group(1))
+        
+        if red_cnt > red_tot:
+            red_tot = red_cnt
+
+        if blue_cnt > blue_tot:
+            blue_tot = blue_cnt
+
+        if green_cnt > green_tot:
+            green_tot = green_cnt
+    
+    if debug:
+        print(f'Game {id = }: {(red_tot * blue_tot * green_tot) =} {red_tot = }, {blue_tot = }, {green_tot = }')
+        
+
+    return red_tot * blue_tot * green_tot
+
 
 def game_validate(g_data, legal_game, debug):
     (id, rounds) = g_data
@@ -84,6 +128,7 @@ def parse_args(arg_list: list[str] | None):
 def main(arg_list: list[str] | None = None):
     args = parse_args(arg_list)
     game_sum = 0
+    power_sum = 0
     #
     # Legal game is  red, bleu, green
     # only 12 red cubes, 13 green cubes, and 14 blue cubes
@@ -95,8 +140,11 @@ def main(arg_list: list[str] | None = None):
             line.strip()
             game = split_game_data(line, debug)
 
-            game_sum += game_validate(game, lgame, debug)
-            print(f'Game ID Sum: {game_sum = }')
+#            game_sum += game_validate(game, lgame, False)
+#            print(f'Game ID Sum: {game_sum = }')
+
+            power_sum += game_powers(game, debug)
+            print(f'Game Power Sum: {power_sum = }')
 
   
 if __name__ == '__main__':
